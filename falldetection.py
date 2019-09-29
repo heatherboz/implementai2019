1bc150dc589ded2c43d30325e9887ebdb38a4b0c
heatherboz <33397779+heatherboz@users.noreply.github.com>
2019-09-28 20:40:24 -0400

    added fall logic

--- falldetection.py
+++ falldetection.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import matplotlib.pyplot as plt
 
 content_type_header = {
     'Content-Type': 'application/json',
@@ -47,7 +48,7 @@ Returns the object of processed data.
 '''
 def get_job(job_id, login_header):
     actual_response = requests.get('https://api.wrnch.ai/v1/jobs/' + job_id, headers=login_header)
-    print('Got Job Response: %s', actual_response.text)
+    print('Got Job Response: ')
     return actual_response.text
 
 
@@ -66,14 +67,14 @@ Checks if a certain job has been processed already, if processed adds to db.
 Void
 '''
 def process_and_add_job(job_id, login_header):
-    job_details = get_job('a3003a2a-104f-4aea-a126-1fb227bf4faf', login_header)
+    job_details = get_job(job_id, login_header)
     response_json = json.loads(job_details)
     if 'message' in response_json:
         return False
     else:
         data = job_details
         is_fall = detect_fall(data)
-        add_to_job_db(location, data, is_fall)
+        # add_to_job_db(location, data, is_fall)
         return True
 
 
@@ -82,19 +83,23 @@ Detects a fall depending on data given.
 
 Returns a boolean if the video is considered a fall.
 '''
-# def detect_fall(data):
-#     print ("detecting fall...")
-#     data = json.loads(data)
-#     frames_len = len(data["frames"])
-#     y_vel1 = data["frames"[0]]["head_pose"]["bbox"]["minY"]
-#     y_vel2 = data["frames"[frames_len -1]]["head_pose"]["bbox"]["minY"]
-#
-#     y_vel = (y_vel2 - yvel1)/frames_len
-#
-#     if y_vel > 5:
-#         return True
+def detect_fall(data):
+    print ("detecting fall...")
+    data = json.loads(data)
+    frames_len = len(data["frames"])
+    for i in range(frames_len):
 
+        if len(data["frames"][i]["persons"]) == 0:
+            print ("no people!")
+        else:
+            ankle_pos = data["frames"][i]["persons"][0]["pose2d"]["joints"][1]
+            hip_pos = data["frames"][i]["persons"][0]["pose2d"]["joints"][5]
+            neck_pos = data["frames"][i]["persons"][0]["pose2d"]["joints"][25]
+            frame_num = i
 
+            if ankle_pos < hip_pos:
+                return True
+    return False
 
 
 '''
@@ -105,11 +110,12 @@ Adds job to database.
 def take_job():
     val = input("Enter location data (x,y):")
 
-# w = open("logins.json", 'r')
-# login_info = json.load(w)
-# username = login_info["wrnchLogin"]["username"]
-# password = login_info["wrnchLogin"]["password"]
-#
-# login_header = login(username, password)
-# job_id = submit_job("data/personfalling.jpg", login_header)
-# process_and_add_job(job_id,login_header)
+w = open("logins.json", 'r')
+login_info = json.load(w)
+username = login_info["wrnchLogin"]["username"]
+password = login_info["wrnchLogin"]["password"]
+
+login_header = login(username, password)
+# job_id = submit_job("data/person_walking.mp4", login_header)
+job_id = ""
+process_and_add_job(job_id,login_header)
